99ede3508e7b161847588483dd07ac7982e5a289
dungphanx <dungphanx@gmail.com>
2016-08-13 23:29:22 +0000

    config production

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,5 @@ module Travelcompanion
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     #config.web_console.whitelisted_ips = '10.0.2.2'
-    config.web_console.automount = true
   end
 end
