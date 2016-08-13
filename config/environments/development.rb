ee859e49ec18e318a0eb8144f395132f01886e24
dungphanx <dungphanx@gmail.com>
2016-08-13 23:54:56 +0000

    Config SENDGRID addons

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,9 +29,18 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   #config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  host = 'localhost:3000'   # Don't use this literally; use your local dev host instead
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  config.action_mailer.delivery_method = :smtp
+  host = 'travelgo.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['app55090161@heroku.com'],
+    :password       => ENV['bgxxu8kw6432'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
